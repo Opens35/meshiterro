2bbe447983893b96753bfc34e49aa2b1100fac06
Opens35 <ryuttti1102@gmail.com>
2021-10-20 19:26:38 +0000

    3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'bf7d0ce97c5142c24586331432c53567f237419f8756b947a7e309059eb3e36c4fee95f95e6221938f687f2e84bcd802de92d27f00083317be2cb002f2e9a31b'
